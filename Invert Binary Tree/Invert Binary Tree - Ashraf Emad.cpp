63c3624938ce17cc82858449714a82c0a71eb432
Ashraf Emad Rabiea <76045436+ashrafemad097@users.noreply.github.com>
2023-01-26 20:13:45 +0200

    Invert Binary Tree C++ Solution

---
+++ Invert Binary Tree/Invert Binary Tree - Ashraf Emad.cpp
@@ -0,0 +1,26 @@
+// Author : Ashraf Emad Alagmawy
+// Time Complexity : O (n) , Space Complexity : O (1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root) {
+            invertTree(root->left);
+            invertTree(root->right);
+            swap(root->left, root->right);
+        }
+        return root;
+    }
+};
