47cabdc507970041ad95f147c6bccf066f926110
Ashraf Emad Rabiea <76045436+ashrafemad097@users.noreply.github.com>
2023-01-26 19:38:26 +0200

    Add Two Integers C++ Solution

---
+++ Add Two Integers/Add Two Integers - Ashraf Emad.cpp
@@ -0,0 +1,9 @@
+// Author : Ashraf Emad Alagmawy
+// Time Complexity : O (1) , Space Complexity : O (1)
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
